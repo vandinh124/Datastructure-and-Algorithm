342ac837bb168b7c33bc1e09ff4b697e88d8bd7b
vandinh124 <vanvdinh124@gmail.com>
2020-10-16 08:40:25 -0700

    search number in the array

---
+++ searchNumber.js
@@ -0,0 +1,28 @@
+function search(arr, val){
+    let min = 0;
+    let max = arr.length - 1;
+
+    while (min <= max){
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = arr[middle];
+
+        if (arr[middle] < val){
+            min = middle + 1;
+        } else if (arr[middle] > val){
+            max = middle - 1;
+        } else {
+            return middle;
+        }
+    }
+    return -1;
+}
+// function search(arr, val){
+//     for (let i =0; i< arr.length; i++){
+//         if (arr[i] === val){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+console.log(search([1,3,4,5,7], 5))
