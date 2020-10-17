2c3b660f2538e3f21e6f2ffc68d78c0eea1358e5
vandinh124 <vanvdinh124@gmail.com>
2020-10-17 09:55:49 -0700

    isOdd recursive

---
+++ isOdd.js
@@ -0,0 +1,15 @@
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1),callback);
+}
+
+// const isOdd = val => val % 2 !== 0;
+console.log(someRecursive([2,4],isOdd))
