ab980809e33ab177403222c396aa220b4b439f91
vandinh124 <vanvdinh124@gmail.com>
2020-11-18 15:53:21 -0800

    added BreadthFirstSearch

--- breadthFirstSearch_BST_BFS.js
+++ breadthFirstSearch_BST_BFS.js
@@ -52,13 +52,32 @@ class BinarySearchTree {
         }
         return false;
     }
+
+    breadthFirstSearch(){
+        var data = [],
+            queue = [],
+            node = this.root;
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
 
 
 var tree = new BinarySearchTree();
 tree.insert(10)
-tree.insert(5)
+tree.insert(6)
 
-tree.insert(7)
-tree.insert(10)
-console.log(tree.find(8))
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+
+console.log(tree.breadthFirstSearch());
+
+
