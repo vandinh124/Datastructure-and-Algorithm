8ffadb15594f07db8226274fa0ea434b6338649f
vandinh124 <vanvdinh124@gmail.com>
2020-11-18 17:04:17 -0800

    added preOrder DepthFirstSearch

---
+++ BST_DFS_BinarySearchTree_DepthFirstSearch.js
@@ -0,0 +1,80 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root===null){
+            this.root = newNode;
+            return this;
+        } 
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                } else {
+                    current = current.left;
+                }
+            } else if(value > current.value){
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                }else {
+                    current = current.right;
+                }
+            }
+        }
+    }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root;
+        var found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if (value > current.value){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    DepthFirstSearch_PreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+}
+
+
+var tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(6)
+
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+
+console.log(tree.DepthFirstSearch_PreOrder());
+
+
