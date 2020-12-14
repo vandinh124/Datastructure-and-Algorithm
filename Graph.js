8851446c47f0c8a4e4f0e4bd5c1859501d0617ff
vandinh124 <vanvdinh124@gmail.com>
2020-12-14 09:25:59 -0800

    added addEdge

--- Graph.js
+++ Graph.js
@@ -9,10 +9,17 @@ class Graph{
         }
         
     }
+
+    addEdge(vertex1, vertex2){
+        this.adjacenyList[vertex1].push(vertex2);
+        this.adjacenyList[vertex2].push(vertex1);
+    }
 }
 
 g = new Graph();
 g.addVertex("Tokyo")
 g.addVertex("San Jose")
+g.addVertex("Dallas")
+g.addEdge("Tokyo", "Dallas")
 // g.adjacenyList["Tokyo"].push("Something")
 console.log(g)
