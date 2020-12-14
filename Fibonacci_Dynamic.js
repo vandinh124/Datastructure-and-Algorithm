fd72ba8799adad4bea12077f3cb7aeb84bff8ab5
vandinh124 <vanvdinh124@gmail.com>
2020-12-13 19:05:56 -0800

    tabulated Fib

--- Fibonacci_Dynamic.js
+++ Fibonacci_Dynamic.js
@@ -1,12 +1,12 @@
-function fib(n, memo=[]){
-    if (memo[n] !== undefined) return memo[n];
-    if(n <=2 ) return 1;
-    var res = fib(n-1, memo) + fib(n-2, memo);
-    // console.log(memo)
-    // console.log(res)
-    memo[n] = res;
-    return res;
-}
+// function fib(n, memo=[]){
+//     if (memo[n] !== undefined) return memo[n];
+//     if(n <=2 ) return 1;
+//     var res = fib(n-1, memo) + fib(n-2, memo);
+//     // console.log(memo)
+//     // console.log(res)
+//     memo[n] = res;
+//     return res;
+// }
 
 // function fib(n, memo=[undefined, 1, 1]){
 //     if (memo[n] !== undefined) return memo[n];
@@ -16,4 +16,13 @@ function fib(n, memo=[]){
 //     return res;
 // }
 
-console.log(fib(10))
+//Tabulated Version
+function fib(n){
+    if(n <=2 ) return 1;
+    var fibNums = [0,1,1]
+    for (var i = 3; i <=n; i++){
+        fibNums[i] = fibNums[i-1] + fibNums[i-2];
+    }
+    return fibNums[n];
+}
+console.log(fib(15))
