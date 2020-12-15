c6f4f33f6902988739ead2e2c9670c14d81638ab
vandinh124 <vanvdinh124@gmail.com>
2020-12-15 08:46:24 -0800

    added removeEdge

--- Graph.js
+++ Graph.js
@@ -14,6 +14,15 @@ class Graph{
         this.adjacenyList[vertex1].push(vertex2);
         this.adjacenyList[vertex2].push(vertex1);
     }
+
+    removeEdge(vertex1, vertex2){
+        this.adjacenyList[vertex1] = this.adjacenyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacenyList[vertex2] = this.adjacenyList[vertex2].filter(
+            v => v !== vertex1
+        );
+    }
 }
 
 g = new Graph();
@@ -22,4 +31,5 @@ g.addVertex("San Jose")
 g.addVertex("Dallas")
 g.addEdge("Tokyo", "Dallas")
 // g.adjacenyList["Tokyo"].push("Something")
+g.removeEdge("Tokyo", "Dallas")
 console.log(g)
