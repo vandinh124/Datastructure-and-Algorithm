be6826345df7c5b8ee208e70508ba3f81c44a1ba
vandinh124 <vanvdinh124@gmail.com>
2020-12-17 08:50:33 -0800

    added DFS recursive

--- Graph.js
+++ Graph.js
@@ -1,51 +1,76 @@
 class Graph{
     constructor(){
-        this.adjacenyList = {};
+        this.adjacencyList = {};
     }
 
     addVertex(vertex){
-        if(!this.adjacenyList[vertex]){
-            this.adjacenyList[vertex] = []
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex] = []
         }
         
     }
 
     addEdge(vertex1, vertex2){
-        this.adjacenyList[vertex1].push(vertex2);
-        this.adjacenyList[vertex2].push(vertex1);
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
     }
 
     removeEdge(vertex1, vertex2){
-        this.adjacenyList[vertex1] = this.adjacenyList[vertex1].filter(
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
             v => v !== vertex2
         );
-        this.adjacenyList[vertex2] = this.adjacenyList[vertex2].filter(
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
             v => v !== vertex1
         );
     }
 
     removeVertex(vertex){
-        while(this.adjacenyList[vertex].length){
-            const adjacentVertex = this.adjacenyList[vertex].pop()
+        while(this.adjacencyList[vertex].length){
+            const adjacencyList = this.adjacencyList[vertex].pop()
             this.removeEdge(vertex, adjacentVertex)
         }
-        delete this.adjacenyList[vertex]
+        delete this.adjacencyList[vertex]
     }
+
+    depthFirstRecursive(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        // function dfs(vertex){
+        //     ...
+        // }
+        // dfs(vertex)
+        
+        (function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            });
+        })(start);
+
+        return result;
+    }
+
 }
 
 g = new Graph();
-g.addVertex("Dallas")
-g.addVertex("Tokyo")
-g.addVertex("Aspen")
-g.addVertex("Los Angeles")
-g.addVertex("Hong Kong")
-g.addEdge("Tokyo", "Dallas")
-g.addEdge("Aspen", "Dallas")
-g.addEdge("Hong Kong", "Tokyo")
-g.addEdge("Hong Kong", "Dallas")
-g.addEdge("Los Angeles", "Hong Kong")
-g.addEdge("Los Angeles", "Aspen")
-// g.adjacenyList["Tokyo"].push("Something")
-g.removeVertex("Hong Kong")
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
 
-console.log(g)
+console.log(g.depthFirstRecursive("A"))
