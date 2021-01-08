00afd52201645fb37ca0e3ba6cd5e37f83e8c555
vandinh124 <vanvdinh124@gmail.com>
2021-01-08 08:15:51 -0800

    add new pattern to GroupTheNumber

--- IK_GroupTheNumbers.js
+++ IK_GroupTheNumbers.js
@@ -1,3 +1,4 @@
+// 2 pointers from left & right
 function GroupTheNumber(arr){
     var leftPointer = 0;
     var rightPointer = arr.length - 1;
@@ -16,4 +17,23 @@ function GroupTheNumber(arr){
     return arr;
 }
 
-console.log(GroupTheNumber([4,9,5,2,9,5,7,10]))
+function GroupTheNumber1(arr){
+    let evenIdx = 0;
+    let currentIdx = 0;
+
+    while (currentIdx < arr.length){
+        if (arr[currentIdx] % 2 == 1){
+            currentIdx++
+        }else {
+            [arr[currentIdx], arr[evenIdx]] = [arr[evenIdx], arr[currentIdx]]
+            currentIdx++
+            evenIdx++
+        }
+    }
+
+    return arr;
+}
+
+
+
+console.log(GroupTheNumber1([4,9,5,2,9,5,7,10]))
